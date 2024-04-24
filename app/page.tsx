0a2b63d0f8c785d6c520e3efed334ebb93e8a336
viksennn <viksen.nonbo@gmail.com>
2024-04-24 13:59:41 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello fish.</p>
+    <div className="h-screen w-full flex gap-2 flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Trolde fishing</h1>
+      <p className="text-xl">Fishing App til at track de største og sejeste fisk</p>
+      <p className="text-sm text-gray-700">Coming soon...</p>
     </div>
   );
 }
