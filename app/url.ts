feba04079b691fbddb6ceea260585484303995ff
viksennn <96552981+viksennn@users.noreply.github.com>
2024-05-07 22:32:47 +0200

    Update url.ts

--- app/url.ts
+++ app/url.ts
@@ -1,1 +1,6 @@
+<<<<<<< Updated upstream
+=======
+// export const PAGE_URL = "https://troldefishing.vercel.app"
+
+>>>>>>> Stashed changes
 export const PAGE_URL = "https://troldefishing.vercel.app/";
