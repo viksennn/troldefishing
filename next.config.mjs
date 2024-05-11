a1a41cb44c33236afdbecf85631c0e7f2a1875e6
viksennn <viksen.nonbo@gmail.com>
2024-05-11 16:43:37 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    pwa: {
+      dest: 'public',
+      register: true,
+      skipWaiting: true,
+    },
+  };
+  
 export default nextConfig;
