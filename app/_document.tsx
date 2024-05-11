102203dbed773333f4ed6fd4e72568b56b374ff7
viksennn <viksen.nonbo@gmail.com>
2024-05-11 16:20:43 +0200

    phone

---
+++ app/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Trolde Fishing App" />
+          <link rel="apple-touch-icon" href="/public/troldefishingimg.png" />
+          <link rel="manifest" href="/app/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
