bebb6d08b8c03465fc525232f59e1b1ec40562df
viksennn <viksen.nonbo@gmail.com>
2024-05-11 16:46:25 +0200

    test

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+
+const nextConfig = {
+  // Your Next.js configuration options here
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+};
+
+module.exports = nextConfig;
