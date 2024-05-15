0beee0c4461248da797dc3bab8b311ed44350aff
viksennn <viksen.nonbo@gmail.com>
2024-05-15 14:33:45 +0200

    Update LoadingFallback.tsx

--- app/components/LoadingFallback.tsx
+++ app/components/LoadingFallback.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 export const FiskerProfileLoading = async () => {
     
     return (
-        <div className="flex gap-5">
+        <div className="flex flex-col lg:flex-row gap-5">
             <Skeleton className="flex py-6 px-4 mt-2 rounded w-full lg:w-[500px] h-[250px]" />
             <Skeleton className="flex py-6 px-4 mt-2 rounded w-full lg:w-[500px] h-[250px]" />
         </div>
