f2416b2b58b813a45316a44c58c8db233901e4b5
viksennn <viksen.nonbo@gmail.com>
2024-05-24 23:32:03 +0200

    Update FiskeBillede.tsx

--- app/components/ui/FiskeBillede.tsx
+++ app/components/ui/FiskeBillede.tsx
@@ -2,11 +2,13 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, Di
 
 export const FiskeBillede = ({img}: any) => {
     return (
-        <Dialog>
-            <DialogTrigger className="p-1 rounded bg-black text-white text-sm mt-1">Vis billede</DialogTrigger>
-            <DialogContent className="bg w-auto lg:min-h-[80vh] lg:max-h-[80vh] p-0">
-                <img src={img} alt="fiskebillede" className="w-full rounded" />
-            </DialogContent>
-        </Dialog>
+        <div className="flex justify-center items-center">
+            <Dialog>
+                <DialogTrigger className="p-2 rounded bg-black text-white text-sm mt-1">Vis billede</DialogTrigger>
+                <DialogContent className="bg w-auto lg:min-h-[80vh] lg:max-h-[80vh] p-0">
+                    <img src={img} alt="fiskebillede" className="w-full rounded" />
+                </DialogContent>
+            </Dialog>
+        </div>
     )
 }
