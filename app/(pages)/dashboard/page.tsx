48122c69b0705587a1787f6ad1d9f503e17dd35f
viksennn <96552981+viksennn@users.noreply.github.com>
2024-08-06 21:16:15 +0200

    Update page.tsx

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -4,28 +4,25 @@ import { connectMongo } from "@/app/data/mongodb";
 import { MediaModel } from "@/app/data/mongoFishingModel";
 import Link from "next/link";
 
-
-
 export default function Home() {
-
   return (
     <div className="p-2 flex flex-col lg:flex-row gap-8 m-0 lg:m-8">
-      <div className="lg:1/3">
+      <div className="lg:w-1/3 lg:sticky lg:top-5 lg:h-screen lg:overflow-y-auto">
         <div className='w-full mb-4 flex items-center gap-2 justify-between pr-5'>
           <p className='text-lg font-bold bg-indigo-500 w-fit py-0.5 px-1 rounded text-white drop-shadow-md'>Seneste Fangst</p>
           <Link href='/seneste-fangster' className='text-indigo-700 text-sm'>Se alle seneste fangster</Link>
         </div>
         <SenesteFangst />
       </div>
-      <div className="w-full lg:w-1/3">
+      <div className="w-full lg:w-1/3 flex-1 lg:overflow-y-auto">
         <div className='w-full mb-4'>
-            <p className='text-lg font-bold bg-indigo-500 w-fit py-0.5 px-1 rounded text-white drop-shadow-md'>Fiske feed</p>
+          <p className='text-lg font-bold bg-indigo-500 w-fit py-0.5 px-1 rounded text-white drop-shadow-md'>Fiske feed</p>
         </div>
         <MainFeed />
       </div>
-      <div className="w-full lg:w-1/3">
+      <div className="lg:w-1/3 lg:sticky lg:top-5 lg:h-screen lg:overflow-y-auto">
         <div className='w-full mb-4'>
-            <p className='text-lg font-bold bg-indigo-500 w-fit py-0.5 px-1 rounded text-white drop-shadow-md'>Catch of the Week</p>
+          <p className='text-lg font-bold bg-indigo-500 w-fit py-0.5 px-1 rounded text-white drop-shadow-md'>Catch of the Week</p>
         </div>
         <div className="border lg:h-[500px] rounded-lg flex-col flex items-center justify-center">
           <p className="text-sm">Kommer snart...</p>
