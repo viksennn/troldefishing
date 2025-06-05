defb4621044c26edc8b4dd3e0ab0a12801c71e5d
viksennn <viksen.nonbo@gmail.com>
2025-06-05 23:36:24 +0200

    Update route.ts

--- app/api/fisk/[id]/fiskedata/route.ts
+++ app/api/fisk/[id]/fiskedata/route.ts
@@ -2,22 +2,29 @@ import { FishingModel } from "@/app/data/mongoFishingModel";
 import { connectMongo } from "@/app/data/mongodb";
 import { NextResponse } from "next/server";
 
-export async function GET(req:any, {params}:any) {
+export async function GET(req: any, { params }: any) {
+  const { searchParams } = new URL(req.url);
+  const skip = parseInt(searchParams.get("skip") || "0");
+  const limit = parseInt(searchParams.get("limit") || "10");
 
-    const { searchParams } = new URL(req.url);
-    const skip = parseInt(searchParams.get('skip') || '0');
-    const limit = parseInt(searchParams.get('limit') || '10');
+  const { id } = params;
 
-    const { id } = params;
-    await connectMongo();
-    const user = await FishingModel.findOne({ _id: id }).select('fiskeData');
+  await connectMongo();
 
-    const reverseData = user?.fiskeData.reverse() || [];
+  const user = await FishingModel.findOne({ _id: id }).select("fiskeData");
 
-    const sliced = user?.fiskeData.slice(skip, skip + limit);
+  if (!user) {
+    return NextResponse.json({ data: [], hasMore: false });
+  }
+  
+  const sorted = [...user.fiskeData].sort((a, b) => {
+    return new Date(b.dato).getTime() - new Date(a.dato).getTime();
+  });
 
-    return NextResponse.json({
-        data: sliced,
-        hasMore: (user?.fiskeData.length || 0) > skip + limit,
-      });
+  const sliced = sorted.slice(skip, skip + limit);
+
+  return NextResponse.json({
+    data: sliced,
+    hasMore: sorted.length > skip + limit,
+  });
 }
